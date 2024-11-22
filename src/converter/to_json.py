8eb50c4358b7dc271decd11fee67131447e4764d
Mike Cook <mike.z.cook@gmail.com>
2024-11-22 10:50:28 -0800

    tree shake

--- src/converter/to_json.py
+++
@@ -1,6 +0,0 @@
-from typing import Dict, Any
-import json
-
-def convert_to_json(lua_data: Dict[str, Any]) -> str:
-    """Convert parsed Lua data to JSON format."""
-    return json.dumps(lua_data, indent=2)
